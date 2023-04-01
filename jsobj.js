ac2898a199e99ede9c9ea949d3f97d2a50ca2138
Rahul <102817224+rahulsalgo@users.noreply.github.com>
2023-04-01 22:24:18 +0530

    commit

---
+++ jsobj.js
@@ -0,0 +1,37 @@
+// let cars={
+//     bmw :3,
+//     audi:4,
+//     santro:6,
+//     servicOfCars()
+//     {
+//        console.log("do you need a service???")
+//     }
+
+// }
+
+// console.log(cars.audi)
+// cars.servicOfCars
+
+// function aboutcars(){
+//      cars={
+//         audi:1,
+//         bmw:2,
+//         santro:3,
+//         serviceOfcars(){
+//             console.log("you need a seervie??")
+//         }
+//      }
+//      return cars;
+
+// }
+// let a=aboutcars();
+// a.serviceOfcars
+ let a =10;
+ function inc(a)
+ {
+    a++ ;
+ }
+
+ inc(a);
+ console.log(a)
+ console.log(inc(a))
